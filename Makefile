83376966147c89b7cc8d9fef437edcf4c858d9af
Thomas Pelletier <thomas@pelletier.codes>
2024-01-26 16:47:42 -0800

    Remove generated sdks

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: install test
+.PHONY: install test dev fmt generate clean update-proto
 
 PYTHON := python
 
@@ -17,3 +17,26 @@ fmt:
 
 test:
 	$(PYTHON) -m unittest discover
+
+.proto:
+	mkdir -p $@
+
+.proto/ring: .proto
+	buf export buf.build/stealthrocket/ring --output=.proto/ring
+
+.proto/dispatch: .proto
+	buf export buf.build/stealthrocket/dispatch --output=.proto/dispatch
+
+update-proto:
+	$(MAKE) clean
+	$(MAKE) generate
+
+generate: .proto/ring .proto/dispatch
+	buf generate --template buf.gen.yaml
+	cd src && find . -type d | while IFS= read -r dir; do touch $$dir/__init__.py; done
+	rm src/__init__.py
+
+clean:
+	rm -rf .proto
+	find . -type f -name '*.pyc' -delete
+	find . -type d -name '__pycache__' -exec rm -r {} \;
--- tests/test_ring.py
+++ tests/test_ring.py
@@ -1,8 +1,24 @@
 import unittest
 
 import dispatch
+import ring.task.v1.service_pb2
+import grpc
 
 
 class TestRing(unittest.TestCase):
     def test_ring(self):
-        self.assertEqual(2, 1)
+        channel = grpc.insecure_channel('localhost:4001')
+        stub = ring.task.v1.service_pb2_grpc.ServiceStub(channel)
+        create_task_input = ring.task.v1.service_pb2.CreateTaskInput(
+            coroutine_uri = "dispatch-http",
+            input = ring.http.v1.service_pb2.HttpInput(
+                url = "https://www.google.com",
+                method = "GET",
+                headers = {
+                    "Accept": "text/html",
+                    "User-Agent": "dispatch"
+                }
+            )
+        )
+        req = ring.task.v1.service_pb2.CreateTasksRequest(tasks=[create_task_input])
+        resp = stub.CreateTasks(req)
