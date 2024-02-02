a7deab915bf3797ac48a9a37af4923eadcfe02ba
Chris O'Hara <cohara87@gmail.com>
2024-02-02 13:59:02 +1000

    Improve docstrings in the durable package

--- src/dispatch/experimental/durable/durable.py
+++ src/dispatch/experimental/durable/durable.py
@@ -4,12 +4,18 @@ from .registry import register_function
 
 
 def durable(fn):
-    """A decorator for a generator that makes it pickle-able."""
+    """A decorator that makes generators serializable."""
     return DurableFunction(fn)
 
 
 class DurableFunction:
-    """A durable generator function that can be pickled."""
+    """A wrapper for a generator function that wraps its generator instances
+    with a DurableGenerator.
+
+    Attributes:
+        fn (FunctionType): A generator function.
+        key (str): A key that uniquely identifies the function.
+    """
 
     def __init__(self, fn: FunctionType):
         self.fn = fn
