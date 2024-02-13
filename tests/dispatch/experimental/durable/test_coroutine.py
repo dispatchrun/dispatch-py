6cd0aca964fc0f613cc52bae3021783acc5bf172
Chris O'Hara <cohara87@gmail.com>
2024-02-13 13:56:07 +1000

    Create failing test for serializing nested coroutines

--- tests/dispatch/experimental/durable/test_coroutine.py
+++ tests/dispatch/experimental/durable/test_coroutine.py
@@ -20,6 +20,12 @@ async def durable_coroutine(a):
     await yields(a)
 
 
+@durable
+async def nested_coroutines(start):
+    await durable_coroutine(start)
+    await durable_coroutine(start + 3)
+
+
 class TestCoroutine(unittest.TestCase):
     def test_pickle(self):
         # Create an instance and run it to the first yield point.
@@ -40,3 +46,30 @@ class TestCoroutine(unittest.TestCase):
         # The original coroutine is not affected.
         assert next(g) == 2
         assert next(g) == 3
+
+    def test_nested(self):
+        self.skipTest("nested coroutines are not working")
+
+        expect = [1, 2, 3, 4, 5, 6]
+        c = nested_coroutines(1)
+        g = c.__await__()
+        assert list(g) == expect
+
+        # Check that the coroutine can be pickled at every yield point.
+        for i in range(len(expect)):
+            # Create a coroutine and advance to the i'th yield point.
+            c = nested_coroutines(1)
+            g = c.__await__()
+            for j in range(i):
+                assert next(g) == expect[j]
+
+            # Create a copy of the coroutine.
+            state = pickle.dumps(c)
+            c2 = pickle.loads(state)
+            g2 = c2.__await__()
+
+            # Check that both the original and the copy yield the
+            # remaining expected values.
+            for j in range(i, len(expect)):
+                assert next(g) == expect[j]
+                assert next(g2) == expect[j]
