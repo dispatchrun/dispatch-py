da09e64e94e87c961aae036b427be379e2499797
Julien Fabre <ju.pryz@gmail.com>
2024-02-20 16:49:21 -0700

    Add example using gather

---
+++ examples/fanout/fanout.py
@@ -0,0 +1,68 @@
+"""Fan-out example using the SDK gather feature
+
+This example demonstrates how to use the SDK to fan-out multiple requests.
+
+Run with:
+
+uvicorn fanout:app
+
+
+You will observe that the get_repo_info calls are executed in parallel.
+
+"""
+import httpx
+from fastapi import FastAPI
+from dispatch.fastapi import Dispatch
+from dispatch import gather
+
+app = FastAPI()
+
+dispatch = Dispatch(app)
+
+
+@dispatch.function
+async def get_repo_info(repo_owner: str, repo_name: str):
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}"
+    api_response = httpx.get(url)
+    api_response.raise_for_status()
+    repo_info = api_response.json()
+    return repo_info
+
+
+@dispatch.function
+async def get_stargazers(repo_info):
+    url = repo_info["stargazers_url"]
+    print(url)
+    response = httpx.get(url)
+    response.raise_for_status()
+    stargazers = response.json()
+    return stargazers
+
+
+@dispatch.function
+async def reduce_stargazers(repos):
+    result = await gather(*[get_stargazers(repo) for repo in repos])
+    reduced_stars = set()
+    print("result len", len(result))
+    for repo in result:
+        print("stars len", len(repo))
+        for stars in repo:
+            reduced_stars.add(stars["login"])
+    return reduced_stars
+
+
+@dispatch.function
+async def main():
+    # Using gather, we fan-out the four following requests.
+    repos = await gather(
+        get_repo_info("stealthrocket", "coroutine"),
+        get_repo_info("stealthrocket", "timecraft"),
+        get_repo_info("stealthrocket", "dispatch-sdk-python"),
+        get_repo_info("stealthrocket", "wzprof"),
+    )
+
+    stars = await reduce_stargazers(repos)
+    print(len(stars))
+
+
+main.dispatch()
---
+++ examples/fanout/test_app.py
@@ -0,0 +1,39 @@
+# This file is not part of the example. It is a test file to ensure the example
+# works as expected during the CI process.
+
+
+import os
+import unittest
+from unittest import mock
+
+from fastapi.testclient import TestClient
+
+from ... import function_service
+from ...test_client import ServerTest
+
+
+class TestFanout(unittest.TestCase):
+    @mock.patch.dict(
+        os.environ,
+        {
+            "DISPATCH_ENDPOINT_URL": "http://function-service",
+            "DISPATCH_API_KEY": "0000000000000000",
+        },
+    )
+    def test_foo(self):
+        from . import app
+
+        server = ServerTest()
+        servicer = server.servicer
+        app.dispatch._client = server.client
+        app.publish._client = server.client
+
+        http_client = TestClient(app.app, base_url="http://dispatch-service")
+        app_client = function_service.client(http_client)
+
+        response = http_client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+        server.execute(app_client)
+
+        self.assertEqual(len(servicer.responses), 1)
