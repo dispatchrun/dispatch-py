1504ee95bd4c3329acd67b85042d2743a35825c6
Chris O'Hara <cohara87@gmail.com>
2024-02-26 09:12:59 +1000

    Update SDK protos

--- src/dispatch/sdk/v1/poll_pb2.pyi
+++ src/dispatch/sdk/v1/poll_pb2.pyi
@@ -16,21 +16,24 @@ from dispatch.sdk.v1 import error_pb2 as _error_pb2
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class Poll(_message.Message):
-    __slots__ = ("coroutine_state", "calls", "max_wait", "max_results")
+    __slots__ = ("coroutine_state", "calls", "max_wait", "max_results", "min_results")
     COROUTINE_STATE_FIELD_NUMBER: _ClassVar[int]
     CALLS_FIELD_NUMBER: _ClassVar[int]
     MAX_WAIT_FIELD_NUMBER: _ClassVar[int]
     MAX_RESULTS_FIELD_NUMBER: _ClassVar[int]
+    MIN_RESULTS_FIELD_NUMBER: _ClassVar[int]
     coroutine_state: bytes
     calls: _containers.RepeatedCompositeFieldContainer[_call_pb2.Call]
     max_wait: _duration_pb2.Duration
     max_results: int
+    min_results: int
     def __init__(
         self,
         coroutine_state: _Optional[bytes] = ...,
         calls: _Optional[_Iterable[_Union[_call_pb2.Call, _Mapping]]] = ...,
         max_wait: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ...,
         max_results: _Optional[int] = ...,
+        min_results: _Optional[int] = ...,
     ) -> None: ...
 
 class PollResult(_message.Message):
