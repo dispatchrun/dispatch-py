116d882b346b23a596e18c5419ecda6ae9e50f9d
Chris O'Hara <cohara87@gmail.com>
2024-03-21 12:47:58 +1000

    Categorize objects/errors from the OpenAI SDK

---
+++ src/dispatch/integrations/openai.py
@@ -0,0 +1,19 @@
+import openai  # type: ignore
+
+from dispatch.integrations.http import http_response_code_status
+from dispatch.status import Status, register_error_type
+
+
+def openai_error_status(error: Exception) -> Status:
+    # See https://github.com/openai/openai-python/blob/main/src/openai/_exceptions.py
+    match error:
+        case openai.APITimeoutError():
+            return Status.TIMEOUT
+        case openai.APIStatusError():
+            return http_response_code_status(error.status_code)
+
+    return Status.TEMPORARY_ERROR
+
+
+# Register base exception.
+register_error_type(openai.OpenAIError, openai_error_status)
