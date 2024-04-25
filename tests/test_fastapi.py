d63ae80ff7ce72642affa975390c0ad47b670ded
Achille Roussel <achille.roussel@gmail.com>
2024-04-24 18:10:29 -0700

    test: drop FastAPI dependency
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -3,34 +3,49 @@ import os
 import pickle
 import struct
 import unittest
-from typing import Any
+from typing import Any, Optional
 from unittest import mock
 
 import fastapi
 import google.protobuf.any_pb2
 import google.protobuf.wrappers_pb2
 import httpx
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
+from cryptography.hazmat.primitives.asymmetric.ed25519 import (
+    Ed25519PrivateKey,
+    Ed25519PublicKey,
+)
 from fastapi.testclient import TestClient
 
+import dispatch
 from dispatch.experimental.durable.registry import clear_functions
 from dispatch.fastapi import Dispatch
 from dispatch.function import Arguments, Error, Function, Input, Output
 from dispatch.proto import _any_unpickle as any_unpickle
 from dispatch.sdk.v1 import call_pb2 as call_pb
 from dispatch.sdk.v1 import function_pb2 as function_pb
-from dispatch.signature import parse_verification_key, public_key_from_pem
+from dispatch.signature import (
+    parse_verification_key,
+    private_key_from_pem,
+    public_key_from_pem,
+)
 from dispatch.status import Status
-from dispatch.test import EndpointClient
+from dispatch.test import DispatchServer, DispatchService, EndpointClient
 
 
-def create_dispatch_instance(app, endpoint):
+def create_dispatch_instance(app: fastapi.FastAPI, endpoint: str):
     return Dispatch(
         app,
         endpoint=endpoint,
         api_key="0000000000000000",
         api_url="http://127.0.0.1:10000",
     )
+
+
+def create_endpoint_client(
+    app: fastapi.FastAPI, signing_key: Optional[Ed25519PrivateKey] = None
+):
+    http_client = TestClient(app)
+    return EndpointClient(http_client, signing_key)
 
 
 class TestFastAPI(unittest.TestCase):
@@ -79,8 +94,7 @@ class TestFastAPI(unittest.TestCase):
                 f"You told me: '{input.input}' ({len(input.input)} characters)"
             )
 
-        client = EndpointClient.from_app(app)
-
+        client = create_endpoint_client(app)
         pickled = pickle.dumps("Hello World!")
         input_any = google.protobuf.any_pb2.Any()
         input_any.Pack(google.protobuf.wrappers_pb2.BytesValue(value=pickled))
@@ -102,6 +116,96 @@ class TestFastAPI(unittest.TestCase):
         self.assertEqual(output, "You told me: 'Hello World!' (12 characters)")
 
 
+signing_key = private_key_from_pem(
+    """
+-----BEGIN PRIVATE KEY-----
+MC4CAQAwBQYDK2VwBCIEIJ+DYvh6SEqVTm50DFtMDoQikTmiCqirVv9mWG9qfSnF
+-----END PRIVATE KEY-----
+"""
+)
+
+verification_key = public_key_from_pem(
+    """
+-----BEGIN PUBLIC KEY-----
+MCowBQYDK2VwAyEAJrQLj5P/89iXES9+vFgrIy29clF9CC/oPPsw3c5D0bs=
+-----END PUBLIC KEY-----
+"""
+)
+
+
+class TestFullFastapi(unittest.TestCase):
+    def setUp(self):
+        self.endpoint_app = fastapi.FastAPI()
+        endpoint_client = create_endpoint_client(self.endpoint_app, signing_key)
+
+        api_key = "0000000000000000"
+        self.dispatch_service = DispatchService(
+            endpoint_client, api_key, collect_roundtrips=True
+        )
+        self.dispatch_server = DispatchServer(self.dispatch_service)
+        self.dispatch_client = dispatch.Client(
+            api_key, api_url=self.dispatch_server.url
+        )
+
+        self.dispatch = Dispatch(
+            self.endpoint_app,
+            endpoint="http://function-service",  # unused
+            verification_key=verification_key,
+            api_key=api_key,
+            api_url=self.dispatch_server.url,
+        )
+
+        self.dispatch_server.start()
+
+    def tearDown(self):
+        self.dispatch_server.stop()
+
+    def test_simple_end_to_end(self):
+        # The FastAPI server.
+        @self.dispatch.function
+        def my_function(name: str) -> str:
+            return f"Hello world: {name}"
+
+        call = my_function.build_call(52)
+        self.assertEqual(call.function.split(".")[-1], "my_function")
+
+        # The client.
+        [dispatch_id] = self.dispatch_client.dispatch([my_function.build_call(52)])
+
+        # Simulate execution for testing purposes.
+        self.dispatch_service.dispatch_calls()
+
+        # Validate results.
+        roundtrips = self.dispatch_service.roundtrips[dispatch_id]
+        self.assertEqual(len(roundtrips), 1)
+        _, response = roundtrips[0]
+        self.assertEqual(any_unpickle(response.exit.result.output), "Hello world: 52")
+
+    def test_simple_missing_signature(self):
+        @self.dispatch.function
+        async def my_function(name: str) -> str:
+            return f"Hello world: {name}"
+
+        call = my_function.build_call(52)
+        self.assertEqual(call.function.split(".")[-1], "my_function")
+
+        [dispatch_id] = self.dispatch_client.dispatch([call])
+
+        self.dispatch_service.endpoint_client = create_endpoint_client(
+            self.endpoint_app
+        )  # no signing key
+        try:
+            self.dispatch_service.dispatch_calls()
+        except httpx.HTTPStatusError as e:
+            assert e.response.status_code == 403
+            assert e.response.json() == {
+                "code": "permission_denied",
+                "message": 'Expected "Signature-Input" header field to be present',
+            }
+        else:
+            assert False, "Expected HTTPStatusError"
+
+
 def response_output(resp: function_pb.RunResponse) -> Any:
     return any_unpickle(resp.exit.result.output)
 
@@ -120,7 +224,7 @@ class TestCoroutine(unittest.TestCase):
             self.app, endpoint="https://127.0.0.1:9999"
         )
         self.http_client = TestClient(self.app)
-        self.client = EndpointClient.from_app(self.app)
+        self.client = create_endpoint_client(self.app)
 
     def execute(
         self, func: Function, input=None, state=None, calls=None
