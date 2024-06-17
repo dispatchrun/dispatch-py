c053298418009f552b2f52c1f7a4366c08a6d459
Achille Roussel <achille.roussel@gmail.com>
2024-06-17 13:21:16 -0700

    fix python version check
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -57,7 +57,7 @@ class TestFastAPI(dispatch.test.TestCase):
         self.sockets = [sock]
         self.uvicorn = uvicorn.Server(config)
         self.runner = Runner()
-        if sys.version_info > (3, 8):
+        if sys.version_info >= (3, 9):
             self.event = asyncio.Event()
         else:
             self.event = asyncio.Event(loop=self.runner.get_loop())
